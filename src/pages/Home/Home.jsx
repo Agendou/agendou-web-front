dff7553574c7eeb8e621d4ad9e7ef5a33847fdc2
Henrique-Crispino <henrique.crispino@sptech.school>
2024-09-30 13:58:02 -0300

    feature: finalização da seção start

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import styles from "./Home.module.css";
 import Header from "../../components/Header/Header"
+import Start from "../../components/LandingPage/Start/Start";
 
 const Home = () => {
   return (
     <>
       <Header/>
-      <section className={styles["section-01"]}>
-        <h1>Otimize seu tempo, maximize o seu potencial.</h1>
-        <h2>Agilize seus agendamentos, organize seu comércio e conquiste mais clientes com a simplicidade que você sempre quis.</h2>
-        
-      </section>
+      <Start/>
     </>
   );
 };
