beaf78608de4a0a265c5ae19d3efb5815eafe564
Henrique-Crispino <henrique.crispino@sptech.school>
2024-10-01 02:49:12 -0300

    feature: finalização da seção sobre nós

--- src/components/LandingPage/AboutUs/AboutUs.jsx
+++ src/components/LandingPage/AboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
     return (
         <section className={style["section"]}>
             <div className={style["left-container"]}>
-                <div>
+                <div className={style["left-title-box"]}>
                     <h1>SOBRE NÓS</h1>
                     <h2 className={style["left-subtitle"]}>Solução Inteligente</h2>
                 </div>
@@ -15,12 +15,19 @@ const AboutUs = () => {
                 </div>
             </div>
             <div className={style["right-container"]}>
-                <p>Sabemos que cada segundo importa no seu negócio, e é por isso que nosso software vai revolucionar a forma como você trabalha!</p>
-                <h1>AGENDOU É SIMPLES!</h1>
-                <h2>O Software que trabalha com você!</h2>
-                <p>É a solução definitiva para empresas e equipes que buscam praticidade na organização do dia a dia. </p>
-                <p>O AGENDOU foi criado para trazer eficiência e tranquilidade para sua empresa, com um agendamento simplificado para o seu negócio!</p>
-                <a href="#"><button className={`btn ${style.button}`}>SAIBA MAIS</button></a>
+                <div className={style["words-container"]}>
+                    <div className={style["right-title-box"]}>
+                        <h1>AGENDOU É SIMPLES!</h1>
+                        <h2>O Software que trabalha com você!</h2>
+                    </div>
+                    <div className={style["paragraph-box"]}>
+                        <p>É a solução definitiva para empresas e equipes que buscam praticidade na organização do dia a dia. </p>
+                        <p>O AGENDOU foi criado para trazer eficiência e tranquilidade para sua empresa, com um agendamento simplificado para o seu negócio!</p>
+                    </div>
+                    <div className={style["button-container"]}>
+                        <a href="#"><button className={`btn ${style.button}`}>SAIBA MAIS</button></a>
+                    </div>
+                </div>
             </div>
         </section>
     )
