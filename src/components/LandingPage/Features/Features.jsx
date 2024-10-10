ab8518b0e1287e8b0a33ef3a250382bef48e3f27
Henrique-Crispino <henrique.crispino@sptech.school>
2024-10-10 18:04:57 -0300

    Refact: correção da responsividade da landing page, adição do respiro e animações

--- src/components/LandingPage/Features/Features.jsx
+++ src/components/LandingPage/Features/Features.jsx
@@ -1,89 +1,107 @@
-import React from "react";
-import styles from "./Features.module.css";
-import { DeviceMobile } from "@phosphor-icons/react";
+import React from 'react';
+import { Grid, Typography, Box } from '@mui/material';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import EventNoteIcon from '@mui/icons-material/EventNote'; 
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import GroupIcon from '@mui/icons-material/Group'; 
+import BarChartIcon from '@mui/icons-material/BarChart'; 
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
-const Features = () => {
+const Features = [
+  {
+    icon: <AccessTimeIcon style={{ fontSize: 70, color: '#00a2e8' }} />,
+    title: 'Agendamento online',
+    description: 'Apenas encaminhe o link e deixe o cliente fazer o agendamento.',
+  },
+  {
+    icon: <EventNoteIcon style={{ fontSize: 70, color: '#00a2e8' }} />,
+    title: 'Agenda',
+    description: 'Gerencie sua agenda de forma fácil e rápida quando e onde estiver.',
+  },
+  {
+    icon: <NotificationsIcon style={{ fontSize: 70, color: '#00a2e8' }} />,
+    title: 'Lembretes automatizados',
+    description: 'Reduza o índice de faltas de última hora em até 70% com os lembretes por e-mail.',
+  },
+  {
+    icon: <GroupIcon style={{ fontSize: 70, color: '#00a2e8' }} />,
+    title: 'Quadro de funcionários',
+    description: 'Tenha uma visão detalhada do rendimento de cada funcionário.',
+  },
+  {
+    icon: <BarChartIcon style={{ fontSize: 70, color: '#00a2e8' }} />,
+    title: 'Relatórios e análises',
+    description: 'Relatórios periódicos e práticos para manter-se sempre informado sobre seu negócio.',
+  },
+  {
+    icon: <AttachMoneyIcon style={{ fontSize: 70, color: '#00a2e8' }} />,
+    title: 'Painel de monitoramento',
+    description: 'Acesse nossa poderosa Dashboard para melhorar sua tomada de decisões.',
+  },
+];
+
+const FeaturesGrid = () => {
   return (
-    <section id="features" className={styles["section"]}>
-      <div className={styles.container}>
-        <div className={styles.titleBox}>
-          <h1>Recursos</h1>
-        </div>
-        <div className={styles.cardsContainer}>
-          <div className={styles.card}>
-            <div className={styles.iconBorder}>
-              <div>
-                <DeviceMobile size={64} color="#010726" />
-              </div>
-            </div>
-            <div className={styles.cardText}>
-              <h2>Agendamento Online</h2>
-              <p>
-                Clientes podem agendar visitas facilmente pelo nosso sistema,
-                sem necessidade de contato direto.
-              </p>
-            </div>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.iconBorder}>
-              <div>
-                <DeviceMobile size={64} color="#010726" />
-              </div>
-            </div>
-            <div className={styles.cardText}>
-              <h2>Agendamento Online</h2>
-              <p>
-                Clientes podem agendar visitas facilmente pelo nosso sistema,
-                sem necessidade de contato direto.
-              </p>
-            </div>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.iconBorder}>
-              <div>
-                <DeviceMobile size={64} color="#010726" />
-              </div>
-            </div>
-            <div className={styles.cardText}>
-              <h2>Agendamento Online</h2>
-              <p>
-                Clientes podem agendar visitas facilmente pelo nosso sistema,
-                sem necessidade de contato direto.
-              </p>
-            </div>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.iconBorder}>
-              <div>
-                <DeviceMobile size={64} color="#010726" />
-              </div>
-            </div>
-            <div className={styles.cardText}>
-              <h2>Agendamento Online</h2>
-              <p>
-                Clientes podem agendar visitas facilmente pelo nosso sistema,
-                sem necessidade de contato direto.
-              </p>
-            </div>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.iconBorder}>
-              <div>
-                <DeviceMobile size={64} color="#010726" />
-              </div>
-            </div>
-            <div className={styles.cardText}>
-              <h2>Agendamento Online</h2>
-              <p>
-                Clientes podem agendar visitas facilmente pelo nosso sistema,
-                sem necessidade de contato direto.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Box sx={{ padding: '2rem 4rem', textAlign: 'center' }}>
+      <Box
+        sx={{
+          width: "100%",
+          textAlign: "center",
+          color: "#010726",
+          marginBottom: "5rem",
+          marginTop: "1rem",
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            fontFamily: "Poppins, sans-serif",
+            fontSize: {
+              xs: "2rem",
+              sm: "2.5rem",
+              md: "3rem",
+            },
+            marginBottom: "1rem",
+          }}
+        >
+          Recursos
+        </Typography>
+      </Box>
+      <Grid container spacing={4} justifyContent="center" alignItems="center">
+        {Features.map((feature, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Box
+              sx={{
+                textAlign: 'center',
+                '&:hover .description': {
+                  opacity: { md: 1 },
+                  transform: { md: 'translateY(0)' },
+                }
+              }}
+            >
+              {feature.icon}
+              <Typography variant="h6" gutterBottom>
+                {feature.title}
+              </Typography>
+              <Typography
+                className="description"
+                variant="body2"
+                color="textSecondary"
+                sx={{
+                  opacity: { xs: 1, md: 0 },
+                  transform: { xs: 'none', md: 'translateY(-20px)' },
+                  transition: { md: 'opacity 0.3s ease, transform 0.3s ease' },
+                }}
+              >
+                {feature.description}
+              </Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
-export default Features;
+
+export default FeaturesGrid;
--- src/components/LandingPage/WhoCanUse/WhoCanUse.jsx
+++ src/components/LandingPage/WhoCanUse/WhoCanUse.jsx
@@ -16,77 +16,132 @@ const WhoCanUse = () => {
     <Box
       sx={{
         height: {
-          xs: 'auto', // Muda para auto para se ajustar ao conteúdo
-          sm: '100vh',
-          md: '100vh',
-          lg: '100vh'
+          xs: "auto",
+          sm: "100vh",
+          md: "120vh",
+          lg: "100vh",
         },
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '2rem',
-        padding: '1rem' // Adiciona padding para evitar que o conteúdo fique muito perto da borda
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "2rem",
+        padding: "1rem",
+        paddingX: {
+          xs: "auto",
+          sm: "6rem",
+          md: "6rem",
+          lg: "6rem",
+          xl: "6rem",
+        },
+        position: "relative", // Define o container pai como relativo
       }}
     >
-      <Box sx={{ width: '100%', textAlign: 'center', color: '#010726' }}>
+      <Box
+        sx={{
+          width: "100%",
+          position: "absolute", // Define o título como absoluto
+          top: "10%", // Ajusta a distância do topo
+          textAlign: "center",
+          color: "#010726",
+          marginBottom: "50px", // Espaçamento consistente abaixo do título
+        }}
+      >
         <Typography
           variant="h1"
           sx={{
+            fontFamily: "Poppins, sans-serif",
             fontSize: {
-              xs: '2rem',  // Ajuste para uma leitura melhor em telas pequenas
-              sm: '2.5rem',
-              md: '3rem',
+              xs: "2rem",
+              sm: "2.5rem",
+              md: "3rem",
             },
-            marginBottom: '1rem' // Adiciona espaço abaixo do título
+            marginBottom: "1rem",
           }}
         >
           Quem pode usar?
         </Typography>
       </Box>
-      <Grid container spacing={2} justifyContent="center" width="100%">
+
+      <Grid
+        container
+        spacing={2}
+        justifyContent="center"
+        width="100%"
+        sx={{ paddingTop: "5rem" }}
+      >
         {[
           {
             img: BarberImg,
             title: "Barbearias",
-            description: "Simplifique os agendamentos e ofereça um atendimento personalizado, melhorando a gestão do seu tempo e a satisfação dos clientes."
+            description:
+              "Simplifique os agendamentos e ofereça um atendimento personalizado, melhorando a gestão do seu tempo e a satisfação dos clientes.",
           },
           {
             img: SaloonImg,
             title: "Salões de beleza",
-            description: "Organize os horários e facilite o agendamento, proporcionando aos clientes uma experiência prática e personalizada."
+            description:
+              "Organize os horários e facilite o agendamento, proporcionando aos clientes uma experiência prática e personalizada.",
           },
           {
             img: AestheticImg,
             title: "Clínicas de estética",
-            description: "Organize os agendamentos da sua clínica com eficiência, oferecendo um atendimento impecável para seus clientes enquanto otimiza suas operações."
-          }
+            description:
+              "Organize os agendamentos da sua clínica com eficiência, oferecendo um atendimento impecável para seus clientes enquanto otimiza suas operações.",
+          },
         ].map((card, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index} sx={{ display: (index === 2) ? { xs: 'none', md: 'block' } : 'block' }}>
-            <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column', padding: '1rem' }}>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            key={index}
+            sx={{
+              display: index === 2 ? { xs: "none", md: "block" } : "block",
+            }}
+          >
+            <Card
+              sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                padding: "1rem",
+              }}
+            >
               <CardActionArea>
                 <CardMedia
                   component="img"
                   height="150"
                   image={card.img}
                   alt={card.title}
-                  sx={{ objectFit: 'cover' }} // Adiciona ajuste de imagem
+                  sx={{ objectFit: "cover" }}
                 />
                 <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography variant="h2" sx={{
-                    fontSize: {
-                      xs: '1.3rem',  // Ajuste para uma leitura melhor em telas pequenas
-                      sm: '1.5rem',
-                      md: '1.8rem',
-                      lg: '2rem',
-                    },
-                    color: '#010726',
-                    marginBottom: '0.5rem',
-                    }}>
+                  <Typography
+                    variant="h2"
+                    sx={{
+                      fontFamily: "Poppins, sans-serif",
+                      fontSize: {
+                        xs: "1.3rem",
+                        sm: "1.5rem",
+                        md: "1.8rem",
+                        lg: "2rem",
+                      },
+                      color: "#010726",
+                      marginBottom: "0.5rem",
+                    }}
+                  >
                     {card.title}
                   </Typography>
-                  <Typography variant="body1" sx={{ color: '#333333', fontSize: { xs: '0.8rem', sm: '1rem' } }}>
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      fontFamily: "Poppins, sans-serif",
+                      color: "#333333",
+                      fontSize: { xs: "0.8rem", sm: "1rem" },
+                    }}
+                  >
                     {card.description}
                   </Typography>
                 </CardContent>
