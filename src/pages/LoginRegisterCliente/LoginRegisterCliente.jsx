741868c0daf05d53782f73aa550d700f8eee9880
Sabrina <sabrina.tulio@sptech.school>
2024-10-14 18:59:27 -0300

    fix: ajustes no css e refatoração da página login/registro do comerciante

--- src/pages/LoginRegisterCliente/LoginRegisterCliente.jsx
+++ src/pages/LoginRegisterCliente/LoginRegisterCliente.jsx
@@ -12,44 +12,46 @@ const LoginRegisterCliente = () => {
     };
 
     return (
-        <div className={`${styles.container} ${styles.containerComerciante} ${isLogin ? '' : styles.registerMode}`}>
-            {isLogin ? (
-                <>
-                    <div className={styles["leftPanel"]}>
-                        <FormLogin switchForm={switchForm} />
-                    </div>
-                    <div className={styles["rightPanel"]}>
-                        <AuthPanel
-                            isLogin={isLogin}
-                            switchForm={switchForm}
-                            titleLogin="Não tem conta?"
-                            titleRegister="Já tem conta?"
-                            messageLogin="Junte-se a nós e comece a agendar com seu profissional favorito!"
-                            messageRegister="Acesse sua conta e continue agendando com seu profissional favorito!"
-                            buttonTextLogin="Entrar"
-                            buttonTextRegister="Cadastre-se"
-                        />
-                    </div>
-                </>
-            ) : (
-                <>
-                    <div className={styles["leftPanel"]}>
-                        <AuthPanel
-                            isLogin={isLogin}
-                            switchForm={switchForm}
-                            titleLogin="Não tem conta?"
-                            titleRegister="Já tem conta?"
-                            messageLogin="Junte-se a nós e comece a agendar com seu profissional favorito!"
-                            messageRegister="Acesse sua conta e continue agendando com seu profissional favorito!"
-                            buttonTextLogin="Entrar"
-                            buttonTextRegister="Cadastre-se"
-                        />
-                    </div>
-                    <div className={styles["rightPanel"]}>
-                        <FormRegisterCliente switchForm={switchForm} />
-                    </div>
-                </>
-            )}
+        <div className={styles.page}>
+            <div className={`${styles.container} ${isLogin ? '' : styles.registerMode}`}>
+                {isLogin ? (
+                    <>
+                        <div className={styles["leftPanel"]}>
+                            <FormLogin switchForm={switchForm} />
+                        </div>
+                        <div className={styles["rightPanel"]}>
+                            <AuthPanel
+                                isLogin={isLogin}
+                                switchForm={switchForm}
+                                titleLogin="Não tem conta?"
+                                titleRegister="Já tem conta?"
+                                messageLogin="Junte-se a nós e comece a agendar com seu profissional favorito!"
+                                messageRegister="Acesse sua conta e continue agendando com seu profissional favorito!"
+                                buttonTextLogin="Entrar"
+                                buttonTextRegister="Cadastre-se"
+                            />
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className={styles["leftPanel"]}>
+                            <AuthPanel
+                                isLogin={isLogin}
+                                switchForm={switchForm}
+                                titleLogin="Não tem conta?"
+                                titleRegister="Já tem conta?"
+                                messageLogin="Junte-se a nós e comece a agendar com seu profissional favorito!"
+                                messageRegister="Acesse sua conta e continue agendando com seu profissional favorito!"
+                                buttonTextLogin="Entrar"
+                                buttonTextRegister="Cadastre-se"
+                            />
+                        </div>
+                        <div className={styles["rightPanel"]}>
+                            <FormRegisterCliente switchForm={switchForm} />
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
     );
 };
