aa9cf9f02b429f939f1498e048059aa66fb2af9c
Henrique-Crispino <henrique.crispino@sptech.school>
2024-10-29 17:52:11 -0300

    Refact: terminando a pagina do agendamento manual

--- src/components/MerchantProfile/ProfileCard/ProfileCard.jsx
+++ src/components/MerchantProfile/ProfileCard/ProfileCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import dayjs from "dayjs";
 import {
   CardContent,
@@ -7,90 +7,268 @@ import {
   Button,
   Grid,
   Card,
+  Box,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
 } from "@mui/material";
-import Calendar from '../CalendarCard/CalendarCard'; // Importamos o novo componente
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker, TimePicker } from "@mui/x-date-pickers";
+import Calendar from "../CalendarCard/CalendarCard";
 import TodayAppointments from "../TodayAppointments/TodayAppointments";
 
 export default function ProfileCard() {
-  const [value, setValue] = React.useState(dayjs("2022-04-17"));
+  const [value, setValue] = useState(dayjs());
+  const [formData, setFormData] = useState({
+    nome: "",
+    telefone: "",
+    servico: "",
+    data: null,
+    hora: null,
+    infoAdicional: "",
+  });
+
+  // Estado para controlar o foco do campo de informações adicionais
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleInputChange = (field, value) => {
+    setFormData({ ...formData, [field]: value });
+  };
+
+  const handleSubmit = () => {
+    console.log("Dados do agendamento:", formData);
+    // Lógica para enviar os dados ao backend
+  };
 
   return (
-    <Grid
-      container
-      spacing={2}
-      style={{ padding: "20px", height: "100vh" }}
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Grid item xs={10} md={8} style={{ margin: "0 auto" }}>
-        <Card
-          sx={{
-            backgroundColor: "#010726",
-            color: "#FFF",
-            height: "500px",
-            width: "100%",
-          }}
-        >
-          <CardContent>
-            <Typography variant="h6">José do Carmo Pereira</Typography>
-            <Typography>Email: jose@gmail.com</Typography>
-            <Typography>Telefone: (11) 96060-0000</Typography>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Grid
+        container
+        spacing={2}
+        style={{ padding: "10px"}}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={10} md={8} style={{ margin: "0 auto" }}>
+          <Card
+            sx={{
+              backgroundColor: "#010726",
+              color: "#FFF",
+              height: "auto",
+              width: "100%",
+              padding: "20px",
+            }}
+          >
+            <CardContent>
+              <Typography variant="h5" gutterBottom>
+                Cadastro de Agendamento Manual
+              </Typography>
 
-            <Typography variant="h6">Serviços</Typography>
-            <Button variant="contained">Luzes</Button>
-            <Button variant="contained">Degradê</Button>
+              <TextField
+                label="Nome"
+                value={formData.nome}
+                onChange={(e) => handleInputChange("nome", e.target.value)}
+                fullWidth
+                margin="normal"
+                InputLabelProps={{ style: { color: "white" } }}
+                InputProps={{
+                  style: { color: "white" },
+                  sx: {
+                    "& .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "#FFF",
+                    },
+                  },
+                }}
+              />
 
-            <TextField
-              label="Informações Adicionais"
-              multiline
-              rows={6}
-              fullWidth
-              margin="normal"
-              InputProps={{
-                style: {
-                  color: "black",
-                  backgroundColor: "white",
-                  height: "240px",
-                },
-              }}
-              InputLabelProps={{
-                style: { color: "black" },
-              }}
-              style={{ marginTop: "20px" }}
-            />
+              <TextField
+                label="Telefone"
+                value={formData.telefone}
+                onChange={(e) => handleInputChange("telefone", e.target.value)}
+                fullWidth
+                margin="normal"
+                InputLabelProps={{ style: { color: "white" } }}
+                InputProps={{
+                  style: { color: "white" },
+                  sx: {
+                    "& .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "#FFF",
+                    },
+                  },
+                }}
+              />
 
-            <Grid container spacing={2} justifyContent="flex-end">
-              <Grid item>
-                <Button variant="outlined">Cancelar</Button>
+              <FormControl fullWidth margin="normal">
+                <InputLabel
+                  style={{ color: "black" }}
+                  sx={{
+                    display: formData.servico ? "none" : "block", // Esconde a label se um serviço estiver selecionado
+                  }}
+                  shrink={false} // Faz a label não encolher
+                >
+                  Serviço
+                </InputLabel>
+                <Select
+                  value={formData.servico}
+                  onChange={(e) => handleInputChange("servico", e.target.value)}
+                  label="Serviço"
+                  sx={{
+                    color: "black", // Exibe a opção selecionada em preto
+                    backgroundColor: "#fff",
+                    "& .MuiSvgIcon-root": {
+                      color: "black", // Ícone da lista (seta) em branco
+                    },
+                  }}
+                >
+                  <MenuItem value="luzes">Luzes</MenuItem>
+                  <MenuItem value="degrade">Degradê</MenuItem>
+                  <MenuItem value="corte">Social</MenuItem>
+                </Select>
+              </FormControl>
+
+              <Box sx={{ display: "flex", gap: "1rem", marginTop: 2 }}>
+                <DatePicker
+                  label="Data do Agendamento"
+                  value={formData.data}
+                  onChange={(date) => handleInputChange("data", date)}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      fullWidth
+                      InputLabelProps={{
+                        sx: {
+                          color: "white", // Cor da label
+                        },
+                      }}
+                      sx={{
+                        "& .MuiOutlinedInput-root": {
+                          "& fieldset": {
+                            borderColor: "white", // Cor da borda
+                          },
+                          "&:hover fieldset": {
+                            borderColor: "white", // Borda ao passar o mouse
+                          },
+                          "&.Mui-focused fieldset": {
+                            borderColor: "white", // Borda ao focar
+                          },
+                          color: "white", // Cor do texto
+                          "& .MuiInputBase-input": {
+                            color: "white", // Cor do texto da input
+                          },
+                        },
+                      }}
+                    />
+                  )}
+                />
+
+                <TimePicker
+                  label="Hora do Agendamento"
+                  value={formData.hora}
+                  onChange={(time) => handleInputChange("hora", time)}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      fullWidth
+                      InputLabelProps={{
+                        sx: {
+                          color: "white", // Cor da label
+                        },
+                      }}
+                      sx={{
+                        "& .MuiOutlinedInput-root": {
+                          "& fieldset": {
+                            borderColor: "white", // Cor da borda
+                          },
+                          "&:hover fieldset": {
+                            borderColor: "white", // Borda ao passar o mouse
+                          },
+                          "&.Mui-focused fieldset": {
+                            borderColor: "white", // Borda ao focar
+                          },
+                          color: "white", // Cor do texto
+                          "& .MuiInputBase-input": {
+                            color: "white", // Cor do texto da input
+                          },
+                        },
+                      }}
+                    />
+                  )}
+                />
+              </Box>
+
+              <TextField
+                label={!isFocused ? "Informações Adicionais" : ""}
+                value={formData.infoAdicional}
+                onChange={(e) =>
+                  handleInputChange("infoAdicional", e.target.value)
+                }
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => {
+                  // Para mostrar o rótulo novamente se o campo estiver vazio
+                  if (formData.infoAdicional === "") {
+                    setIsFocused(false);
+                  }
+                }}
+                multiline
+                rows={4}
+                fullWidth
+                margin="normal"
+                InputProps={{
+                  style: {
+                    color: "black",
+                    backgroundColor: "white",
+                  },
+                }}
+                InputLabelProps={{
+                  style: { color: "black" },
+                }}
+              />
+
+              <Grid
+                container
+                spacing={2}
+                justifyContent="flex-end"
+                sx={{ marginTop: 2 }}
+              >
+                <Grid item>
+                  <Button variant="outlined" onClick={() => setFormData({})}>
+                    Cancelar
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}
+                  >
+                    Salvar
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item>
-                <Button variant="contained" color="primary">
-                  Salvar
-                </Button>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
 
-      <Grid item xs={10} md={3} style={{ margin: "0 auto" }}>
-        <Card
-          style={{
-            height: "310px",
-            color: "black",
-            backgroundColor: "#010726",
-            width: "125%",
-            marginLeft: "-40px",
-          }}
-        >
-          <CardContent>
-            <Calendar value={value} onChange={setValue} />
-          </CardContent>
-        </Card>
+        <Grid item xs={10} md={3} style={{ margin: "0 auto" }}>
+          <Card
+            style={{
+              height: "325px",
+              color: "black",
+              backgroundColor: "#010726",
+              width: "125%",
+              marginLeft: "-40px",
+            }}
+          >
+            <CardContent>
+              <Calendar value={value} onChange={setValue} />
+            </CardContent>
+          </Card>
 
-        {/* Utilize o novo componente ScheduleCard */}
-        <TodayAppointments />
+          <TodayAppointments />
+        </Grid>
       </Grid>
-    </Grid>
+    </LocalizationProvider>
   );
 }
