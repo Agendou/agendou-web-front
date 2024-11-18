b6465143e2dcc2789c021e6dcc82b43c9fa594d2
Sabrina <sabrina.tulio@sptech.school>
2024-11-18 20:17:11 -0300

    fix: ajustando arquivo de api

--- src/api.js
+++ src/api.js
@@ -4,8 +4,7 @@ const api = axios.create({
     baseURL: 'http://localhost:8080',
     headers: {
         'Content-Type': 'application',
-    },
-    withCredentials: false
+    }
 });
 
 export default api;
