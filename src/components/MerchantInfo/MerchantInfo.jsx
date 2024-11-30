3b46dd27f16ff146134664d4fc0077ee34f37c37
Sabrina <sabrina.tulio@sptech.school>
2024-11-30 00:38:40 -0300

    feat: conectando formulario de atualizar perfil do comerciante.

--- src/components/MerchantInfo/MerchantInfo.jsx
+++ src/components/MerchantInfo/MerchantInfo.jsx
@@ -9,6 +9,8 @@ import {
   Paper,
 } from "@mui/material";
 import { Visibility, VisibilityOff, Lock, Edit } from "@mui/icons-material";
+import { toast } from "react-toastify";
+import api from "../../services/api";
 
 const MerchantInfo = () => {
   const [showPassword, setShowPassword] = React.useState(false);
@@ -16,6 +18,96 @@ const MerchantInfo = () => {
   const handleTogglePassword = () => {
     setShowPassword(!showPassword);
   };
+
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const [formData, setFormData] = React.useState({
+    cnpj: "",
+    nomeEmpresa: "",
+    representante: "",
+    telefone: "",
+    email: "",
+    senha: "",
+  });
+
+  const dataMerchantInfo = async () => {
+    const token = localStorage.getItem("token");
+    const userIdEmpresa = localStorage.getItem("userIdEmpresa");
+
+    if (!token) {
+      toast.error("Você precisa estar logado para acessar essa página");
+      return;
+    }
+
+    try {
+      const response = await api.get(`/empresas/${userIdEmpresa}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200) {
+        setFormData({
+          cnpj: response.data.cnpj || "",
+          nomeEmpresa: response.data.nomeEmpresa || "",
+          representante: response.data.representante || "",
+          telefone: response.data.telefone || "",
+          email: response.data.email || "",
+          senha: "",
+        });
+      } else {
+        toast.error("Erro ao buscar informações da empresa");
+      }
+    } catch (error) {
+      console.error("Erro na requisição: ", error);
+      toast.error("Erro ao carregar os dados. Tente novamente.");
+    }
+  };
+
+  const handleSubmit = async () => {
+    const token = localStorage.getItem("token");
+    const userIdEmpresa = localStorage.getItem("userIdEmpresa");
+
+    if (!token) {
+      toast.error("Você precisa estar logado para acessar essa página")
+    }
+
+    const perfilAtualizado = {
+      cnpj: formData.cnpj,
+      nomeEmpresa: formData.nomeEmpresa,
+      representante: formData.representante,
+      telefone: formData.telefone,
+      email: formData.email,
+      senha: formData.senha,
+    };
+
+    setIsLoading(true);
+
+    try {
+      const response = await api.put(`/empresas/atualizar/${userIdEmpresa}`, perfilAtualizado, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 200) {
+        toast.success("Perfil atualizado com sucesso!");
+      } else {
+        toast.error("Erro ao atualizar perfil");
+      }
+
+    } catch (error) {
+      console.error("Erro na requisição: " + error);
+      toast.error("Erro de conexão com o servidor. Tente novamente.");
+
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    dataMerchantInfo();
+  }, []);
 
   return (
     <Paper
@@ -49,6 +141,8 @@ const MerchantInfo = () => {
         <TextField
           label="CNPJ"
           variant="outlined"
+          disabled
+          value={formData.cnpj}
           fullWidth
           InputProps={{
             endAdornment: (
@@ -60,7 +154,7 @@ const MerchantInfo = () => {
           }}
           InputLabelProps={{ style: { color: "#fff" } }}
           sx={{
-            backgroundColor: "#808080", 
+            backgroundColor: "#808080",
             borderRadius: 2,
             "& .MuiOutlinedInput-root": {
               "& fieldset": {
@@ -74,13 +168,14 @@ const MerchantInfo = () => {
               },
             },
           }}
-          value="4990988600164"
-          disabled
         />
 
         <TextField
           label="Nome Empresa"
           variant="outlined"
+          value={formData.nomeEmpresa}
+          onChange={(e) => setFormData({ ...formData, nomeEmpresa: e.target.value })
+          }
           fullWidth
           InputLabelProps={{ style: { color: "#ccc" } }}
           sx={{
@@ -99,12 +194,15 @@ const MerchantInfo = () => {
               },
             },
           }}
-          defaultValue="Beto Barber King"
+          defaultValue="Nome empresa"
         />
 
         <TextField
           label="Representante Legal"
           variant="outlined"
+          value={formData.representante}
+          onChange={(e) => setFormData({ ...formData, representante: e.target.value })
+          }
           fullWidth
           InputLabelProps={{ style: { color: "#ccc" } }}
           sx={{
@@ -123,12 +221,15 @@ const MerchantInfo = () => {
               },
             },
           }}
-          defaultValue="Humberto Souza"
+          defaultValue="Nome"
         />
 
         <TextField
           label="Telefone"
           variant="outlined"
+          value={(formData.telefone)}
+          onChange={(e) => setFormData({ ...formData, telefone: e.target.value })
+          }
           fullWidth
           InputLabelProps={{ style: { color: "#ccc" } }}
           sx={{
@@ -154,6 +255,9 @@ const MerchantInfo = () => {
         <TextField
           label="Email"
           variant="outlined"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target })
+          }
           fullWidth
           InputLabelProps={{ style: { color: "#ccc" } }}
           sx={{
@@ -180,6 +284,9 @@ const MerchantInfo = () => {
           variant="outlined"
           type={showPassword ? "text" : "password"}
           fullWidth
+          value={formData.senha}
+          onChange={(e) => setFormData({ ...formData, senha: e.target.value })
+          }
           InputLabelProps={{ style: { color: "#ccc" } }}
           sx={{
             borderRadius: 2,
@@ -233,6 +340,7 @@ const MerchantInfo = () => {
           </Button>
           <Button
             variant="contained"
+            onClick={handleSubmit}
             sx={{
               backgroundColor: "#0066CC",
               "&:hover": {
@@ -240,7 +348,7 @@ const MerchantInfo = () => {
               },
             }}
           >
-            Salvar alterações
+            {isLoading ? "Salvando..." : "Salvar alterações"}
           </Button>
         </Box>
       </Box>
